9732c2cdcef4d6ec599129ceba6b7b28cb716407
Qing Yang <qyang.kevin@gmail.com>
2019-10-21 18:11:55 -0600

    Implemented hash functions, added corresponding tests

--- hash_functions.py
+++ hash_functions.py
@@ -1,4 +1,6 @@
-import sys
+import os
+import argparse
+
 
 def h_ascii(key, N):
     '''
@@ -9,11 +11,13 @@ def h_ascii(key, N):
         s += ord(key[i])
     return s % N
 
+
 def h_rolling(key, N, p=53, m=2**64):
     '''
-    p a prime number roughly equal to the number of characters in the input alphabe
-    m should be a large number, since the probability of two random strings colliding is
-    about 1/m. Sometimes m=2^64 is chosen
+    p a prime number roughly equal to the number of characters in the
+    input alphabe
+    m should be a large number, since the probability of two random strings
+    colliding is about 1/m. Sometimes m=2^64 is chosen
     '''
     s = 0
     for i in range(len(key)):
@@ -21,10 +25,46 @@ def h_rolling(key, N, p=53, m=2**64):
     s = s % m
     return s % N
 
-if __name__ == '__main__':
 
-    for l in open(sys.argv[1]):
-        if sys.argv[2] == 'ascii':
-            print(h_ascii(l, 1000))
-        elif sys.argv[2] == 'rolling':
-            print(h_rolling(l, 1000))
+def h_sedgwicks(key, N):
+    '''
+    A hash function mentioned in Robert Sedgwicks's Algorithm in C. Optimized
+    to speed up the process
+    '''
+    b = 378551
+    a = 63689
+    s = 0
+    for i in range(len(key)):
+        s = s * a + ord(key[i])
+        a = a * b
+    return s % N
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Hash functions')
+
+    parser.add_argument('input', type=str,
+                        help='Input filename')
+
+    parser.add_argument('algorithm', type=str,
+                        help='Hash functions')
+
+    parser.add_argument('size', type=int,
+                        help='Size of hash table')
+
+    args = parser.parse_args()
+
+    if not os.path.exists(args.input):
+        raise FileNotFoundError('Invalid inputfile')
+    if args.algorithm not in ['ascii', 'rolling', 'sedgwicks']:
+        raise ValueError('Invalid hash algorithm selection')
+    if args.size <= 0:
+        raise ValueError('Invalid hash table sizes')
+
+    for l in open(args.input):
+        if args.algorithm == 'ascii':
+            print(h_ascii(l, args.size))
+        elif args.algorithm == 'rolling':
+            print(h_rolling(l, args.size))
+        elif args.algorithm == 'sedgwicks':
+            print(h_sedgwicks(l, args.size))
--- hash_tables.py
+++ hash_tables.py
@@ -1,29 +1,41 @@
+import argparse
+import sys
 import hash_functions
+import time
+import random
+import os
+
 
 class LinearProbe:
     def __init__(self, N, hash_method):
         self.hash = hash_method
         self.N = N
-        self.T = [ None for i in range(N) ]
+        self.T = [None for i in range(N)]
         self.M = 0
 
     def add(self, key, value):
+        '''
+        Function to add key and value to the hash table
+        '''
         hash_slot = self.hash(key, self.N)
 
         for i in range(self.N):
             test_slot = (hash_slot + i) % self.N
-            if self.T[test_slot] == None:
+            if self.T[test_slot] is None:
                 self.T[test_slot] = (key, value)
                 self.M += 1
                 return True
         return False
 
     def search(self, key):
+        '''
+        Function to search for key and value in the hash table
+        '''
         hash_slot = self.hash(key, self.N)
 
         for i in range(self.N):
             test_slot = (hash_slot + i) % self.N
-            if self.T[test_slot] == None:
+            if self.T[test_slot] is None:
                 return None
             if self.T[test_slot][0] == key:
                 return self.T[test_slot][1]
@@ -34,25 +46,113 @@ class ChainHashTable:
     def __init__(self, N, hash_method):
         self.hash = hash_method
         self.N = N
-        self.T = [ [] for i in range(N) ]
+        self.T = [[] for i in range(N)]
         self.M = 0
 
     def add(self, key, value):
+        '''
+        Function to add key and value to the hash table
+        '''
         hash_slot = self.hash(key, self.N)
-        self.T[hash_slot].append((key,value))
+        self.T[hash_slot].append((key, value))
         self.M += 1
         return True
 
     def search(self, key):
+        '''
+        Function to search for key and value in the hash table
+        '''
         hash_slot = self.hash(key, self.N)
 
-        for k,v in self.T[hash_slot]:
+        for k, v in self.T[hash_slot]:
             if key == k:
                 return v
         return None
 
 
+class QuadraticProbing:
+    def __init__(self, N, hash_method):
+        self.hash = hash_method
+        self.N = N   # Size of the hash
+        self.T = [None for i in range(N)]
+        self.M = 0   # Number of elements
+
+    def quadraticProbing(self, pos):
+        # limit variable is used to restrict the function from going into
+        # infinite loop limit is useful when the table is 80% full
+        limit = 50
+        i = 1
+        # start a loop to find the pos
+        while i <= limit:
+            # calculate new position by quadratic probing
+            newPos = pos + (i**2)
+            newPos = newPos % self.N
+            # if newPos is empty then break out of loop and return new Position
+            if self.T[newPos] is None:
+                break
+            else:
+                # as the position is not empty increase i
+                i += 1
+        return newPos
+
+    def add(self, key, value):
+        '''
+        Function to add key and value to the hash table
+        '''
+        hash_slot = self.hash(key, self.N)
+
+        # checking if the position is empty
+        if self.T[hash_slot] is None:
+            # empty position found , store the element and print the message
+            self.T[hash_slot] = (key, value)
+            self.M += 1
+        # collision occured hence we do linear probing
+        else:
+            hash_slot = self.quadraticProbing(hash_slot)
+            self.T[hash_slot] = (key, value)
+            self.M += 1
+        return True
+
+    # method that searches for an element in the table
+    # returns position of element if found
+    # else returns False
+    def search(self, key):
+        '''
+        Function to search for key and value in the hash table
+        '''
+        hash_slot = self.hash(key, self.N)
+        if self.T[hash_slot] is None:
+            return None
+        elif self.T[hash_slot][0] == key:
+            return (self.T[hash_slot][1])
+        # if element is not found at position returned hash function
+        # then we search element using quadratic probing
+        else:
+            limit = 50
+            i = 1
+            newSlot = hash_slot
+            # start a loop to find the position
+            while i <= limit:
+                # calculate new position by quadratic probing
+                newSlot = hash_slot + (i**2)
+                newSlot = newSlot % self.N
+                # if element at newPosition is equal to the required element
+                if self.T[newSlot] is None:
+                    return None
+                    break
+                elif self.T[newSlot][0] == key:
+                    return (self.T[newSlot][1])
+                    break
+                else:
+                    # as the position is not empty increase i
+                    i += 1
+            return None
+
+
 def reservoir_sampling(new_val, size, V):
+    '''
+    Function to subsample the given reservoir by provided size
+    '''
     if len(V) < size:
         V.append(new_val)
     else:
@@ -61,36 +161,65 @@ def reservoir_sampling(new_val, size, V):
             V[j] = new_val
 
 
-
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        description='self-defined hash table objects')
+    parser.add_argument('size', type=int, help='Hash table size')
+    parser.add_argument('hash_function', type=str, help='Hash function'
+                        'ascii | rolling | sedgwicks')
+    parser.add_argument('collision_strategy', type=str,
+                        help='Choose collision handling strategy'
+                        'linear | chain | quadratic')
+    parser.add_argument('input', type=str, help='Input filename')
+    parser.add_argument('num_key', type=int, help='Number of key to add')
 
-    N = int(sys.argv[1])
-    hash_alg = sys.argv[2]
-    collision_strategy = sys.argv[3]
-    data_file_name= sys.argv[4]
-    keys_to_add = int(sys.argv[5])
+    args = parser.parse_args()
+
+    if args.size <= 0:
+        raise ValueError('Invalid hash table sizes')
+    if not os.path.exists(args.input):
+        raise FileNotFoundError('Input file not found')
+    if args.hash_function not in ['ascii', 'rolling', 'sedgwicks']:
+        raise ValueError('Specified hash function does not exist')
+    if args.collision_strategy not in [
+                        'linear', 'chain', 'quadratic']:
+        raise ValueError('Specified collision strategy does not exist')
+    if args.num_key <= 0:
+        raise ValueError('Invalid key number')
+
+    N = args.size
+    hash_alg = args.hash_function
+    collision_strategy = args.collision_strategy
+    data_file_name = args.input
+    keys_to_add = args.num_key
 
     ht = None
 
     if hash_alg == 'ascii':
 
         if collision_strategy == 'linear':
-            ht = LinearProbe(N, hash_functions.h_ascii_sum)
+            ht = LinearProbe(N, hash_functions.h_ascii)
         elif collision_strategy == 'chain':
-            ht = ChainHashTable(N, hash_functions.h_ascii_sum)
+            ht = ChainHashTable(N, hash_functions.h_ascii)
+        elif collision_strategy == 'quadratic':
+            ht = QuadraticProbing(N, hash_functions.h_ascii)
 
     elif hash_alg == 'rolling':
 
         if collision_strategy == 'linear':
-            ht = LinearProbe(N, hash_functions.h_polynomial_rolling)
+            ht = LinearProbe(N, hash_functions.h_rolling)
         elif collision_strategy == 'chain':
-            ht = ChainHashTable(N, hash_functions.h_polynomial_rolling)
+            ht = ChainHashTable(N, hash_functions.h_rolling)
+        elif collision_strategy == 'quadratic':
+            ht = QuadraticProbing(N, hash_functions.h_rolling)
 
-    elif hash_alg == 'python':
+    elif hash_alg == 'sedgwicks':
         if collision_strategy == 'linear':
-            ht = LinearProbe(N, hash_functions.h_python)
+            ht = LinearProbe(N, hash_functions.h_sedgwicks)
         elif collision_strategy == 'chain':
-            ht = ChainHashTable(N, hash_functions.h_python)
+            ht = ChainHashTable(N, hash_functions.h_sedgwicks)
+        elif collision_strategy == 'quadratic':
+            ht = QuadraticProbing(N, hash_functions.h_sedgwicks)
 
     keys_to_search = 100
     V = []
@@ -103,7 +232,6 @@ if __name__ == '__main__':
         print('add', ht.M/ht.N, t1 - t0)
         if ht.M == keys_to_add:
             break
-
 
     for v in V:
         t0 = time.time()
